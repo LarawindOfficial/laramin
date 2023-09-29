b2264986a0c195a83ad132921fc9868e331c0cb4
Tristan Elliott <declanjamesdevilliers2722@gmail.com>
2023-09-30 01:05:43 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+echo "Installing the application..."
+
 # Install Composer Dependancies
+echo "Installing Composer Dependancies..."
 composer install
 
 # Install Node Dependancies
+echo "Installing Node Dependancies..."
 npm install
 
+# Copy the enviroment file
+echo "Copying the enviroment file..."
+cp .env.example .env
+
 # Generate the enviroment key
+echo "Generating the enviroment key..."
 php artisan key:generate
 
 # Migrate the database & seed
+echo "Migrating and seeding the database..."
 php artisan migrate:fresh --seed
