c2c57b0f323f87925bc29238a70e3af7fd9067ad
Tristan Elliott <121314838+TheDevStallion96@users.noreply.github.com>
2023-09-30 05:12:44 +0200

    Create create-new-permission-form.blade.php

---
+++ resources/views/admin/permissions/partials/create-new-permission-form.blade.php
@@ -0,0 +1,13 @@
+
+            <form action="{{ route('permissions.store') }}" method="POST" class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                @csrf
+                <x-input type="text" name="name" value="{{ old('name') }}" label="{{ __('Slug') }}"></x-input>
+                <x-input type="text" name="display_name" value="{{ old('display_name') }}" label="{{ __('Name') }}"></x-input>
+                <x-input type="text" name="description" value="{{ old('description') }}" label="{{ __('Description') }}"></x-input>
+<div>
+                    <x-primary-button>
+                        {{ __('Create') }}
+                    </x-primary-button>
+                </div>
+            </form>
+
