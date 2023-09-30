b945e73a105023880681b7919a45390b1cd82f8c
Tristan Elliott <121314838+TheDevStallion96@users.noreply.github.com>
2023-09-30 05:25:39 +0200

    Update edit.blade.php

--- resources/views/admin/permissions/edit.blade.php
+++ resources/views/admin/permissions/edit.blade.php
@@ -1,46 +1,14 @@
-
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Update Permission') }}
-        </h2>
+        <x-page.header.title>{{ __('Update Permission') }}</x-page.header.title>
     </x-slot>
 
     <x-slot name="actions">
-        <a href="{{ route('permissions.index') }}" class="button return-button">
-            {{ __('Return') }}
-        </a>
-        <a href="{{ route('permissions.show', $permission->id) }}" class="button return-button">
-            {{ __('View Permission') }}
-        </a>
+        <x-page.actions.return :route="route('permissions.index')" />
+        <x-page.actions.show :route="route('permissions.show', $permission->id)" />
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <form action="{{ route('permissions.edit', $permission->id) }}" method="PATCH" class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
-                @csrf
-                <div class="mb-3">
-                    <x-input-label for="name" :value="__('Slug')" />
-                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="username" />
-                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                </div>
-                <div class="mb-3">
-                    <x-input-label for="display_name" :value="__('Name')" />
-                    <x-text-input id="display_name" class="block mt-1 w-full" type="text" name="display_name" :value="old('display_name')" required autofocus autocomplete="username" />
-                    <x-input-error :messages="$errors->get('display_name')" class="mt-2" />
-                </div>
-                <div class="mb-3">
-                    <x-input-label for="description" :value="__('Description')" />
-                    <x-text-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required autofocus autocomplete="username" />
-                    <x-input-error :messages="$errors->get('description')" class="mt-2" />
-                </div>
-
-                <div>
-                    <x-primary-button>
-                        {{ __('Update') }}
-                    </x-primary-button>
-                </div>
-            </form>
-        </div>
-    </div>
+    <x-page.content.container>
+        @include('admin.permissions.partials.edit-permission-form')
+    </x-page.content.container>
 </x-app-layout>
