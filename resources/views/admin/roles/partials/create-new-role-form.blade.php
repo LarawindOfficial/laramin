b8cc5b4bf23c0871d093c2480c8ce15fdfe4442c
Tristan Elliott <121314838+TheDevStallion96@users.noreply.github.com>
2023-09-30 04:22:25 +0200

    Update create-new-role-form.blade.php

--- resources/views/admin/roles/partials/create-new-role-form.blade.php
+++ resources/views/admin/roles/partials/create-new-role-form.blade.php
@@ -1,1 +1,22 @@
-Hello
+
+            <form action="{{ route('roles.store') }}" method="POST" class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                @csrf
+                <x-input type="text" name="name" value="{{ old('name') }}" label="{{ __('Slug') }}"></x-input>
+                <x-input type="text" name="display_name" value="{{ old('display_name') }}" label="{{ __('Name') }}"></x-input>
+                <x-input type="text" name="description" value="{{ old('description') }}" label="{{ __('Description') }}"></x-input>
+
+                <div class="grid grid-cols-4 gap-4">
+                    @foreach ($permissions as $permission)
+                        <div class="">
+                            <input type="checkbox" id="{{ $permission->name }}" name="permissions[]" value="{{ $permission->name }}">
+                            <label class="text-white" for="permissions">{{ $permission->display_name }}</label>
+                        </div>
+                    @endforeach
+                </div>
+
+                <div>
+                    <x-primary-button>
+                        {{ __('Create') }}
+                    </x-primary-button>
+                </div>
+            </form>
