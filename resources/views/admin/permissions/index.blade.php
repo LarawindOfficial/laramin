9d43c8e16fe1c4c19d233112538455ee7098e564
Tristan Elliott <121314838+TheDevStallion96@users.noreply.github.com>
2023-09-30 04:15:10 +0200

    Update index.blade.php

--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -1,5 +1,27 @@
 <x-app-layout>
     <x-slot name="header">
+        <x-page.header.title>{{ __('Permissions') }}</x-page.header.title>
+    </x-slot>
+
+    <x-slot name="actions">
+        @role('superadministrator')
+            <x-page.actions.create :route="route('permissions.create')" />
+        @endrole
+    </x-slot>
+
+    <x-page.content.container>
+        @if ($permissions->count() < 0)
+            <x-card>
+                <span class="text-white">Looks like there are no permissions to show at the moment.</span>
+            </x-card>
+        @else
+            @include('admin.permissions.partials.permissions-table')
+        @endif
+    </x-page.content.container>
+</x-app-layout>
+
+<x-app-layout>
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Permissions') }}
         </h2>
