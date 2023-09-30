e4f8c420bb16d357ba0bcb33230269146e344716
Tristan Elliott <121314838+TheDevStallion96@users.noreply.github.com>
2023-09-30 04:55:45 +0200

    Update edit-role-form.blade.php

--- resources/views/admin/roles/partials/edit-role-form.blade.php
+++ resources/views/admin/roles/partials/edit-role-form.blade.php
@@ -1,1 +1,23 @@
-Hello
+
+            <form action="{{ route('roles.update', $role->id) }}" method="PATCH" class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                @csrf
+                <x-input type="text" name="name" value="{{ old('name') }}" label="{{ __('Slug') }}"></x-input>
+                <x-input type="text" name="display_name" value="{{ old('display_name') }}" label="{{ __('Name') }}"></x-input>
+                <x-input type="text" name="description" value="{{ old('description') }}" label="{{ __('Description') }}"></x-input>
+
+                <div class="grid grid-cols-4 gap-4">
+                    @foreach ($permissions as $permission)
+                        <div class="">
+                            <input type="checkbox" id="{{ $permission->name }}" name="permissions[]" value="{{ old('name') }}">
+                            <label class="text-white" for="permissions">{{ $permission->display_name }}</label>
+                        </div>
+                    @endforeach
+                </div>
+
+                <div>
+                    <x-primary-button>
+                        {{ __('Update Permission') }}
+                    </x-primary-button>
+                </div>
+            </form>
+
